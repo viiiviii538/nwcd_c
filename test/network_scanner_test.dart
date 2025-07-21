6c2c45c94937219c787cd830d3c452715d72a117
viiiviii538 <viiiviii538@gmail.com>
2025-07-22 00:13:25 +0900

    Add host limit to ping sweep

--- test/network_scanner_test.dart
+++ test/network_scanner_test.dart
@@ -11,4 +11,27 @@ void main() {
     expect(calculateNetwork('192.168.1.5', 24), '192.168.1.0');
     expect(calculateNetwork('10.0.1.50', 16), '10.0.0.0');
   });
+
+  test('pingSweepForTest limits large networks', () async {
+    final pinged = <String>[];
+    await pingSweepForTest(
+      '192.168.0.0/16',
+      pingAddress: (ip) async => pinged.add(ip),
+      readArpTable: () async => <NetworkDevice>[],
+      maxHosts: 10,
+    );
+    expect(pinged.length, 10);
+    expect(pinged.first, '192.168.0.1');
+    expect(pinged.last, '192.168.0.10');
+  });
+
+  test('pingSweepForTest scans full range when small', () async {
+    final pinged = <String>[];
+    await pingSweepForTest(
+      '10.0.0.0/30',
+      pingAddress: (ip) async => pinged.add(ip),
+      readArpTable: () async => <NetworkDevice>[],
+    );
+    expect(pinged, ['10.0.0.1', '10.0.0.2']);
+  });
 }
