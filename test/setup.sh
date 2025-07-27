9463a316552ae27e8e5c7dd3c30d28daa12d3678
viiiviii538 <viiiviii538@gmail.com>
2025-07-27 20:19:56 +0900

    s

---
+++ test/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "🌱 Flutterをインストール中..."
+if [ ! -d /usr/local/flutter ]; then
+  git clone https://github.com/flutter/flutter.git -b stable /usr/local/flutter
+else
+  echo "✅ Flutterは既に入ってるよ"
+fi
+
+export PATH="/usr/local/flutter/bin:/usr/local/flutter/bin/cache/dart-sdk/bin:$PATH"
+
+echo "🔧 バージョン確認中..."
+flutter --version
+
+echo "📦 依存取得中..."
+flutter pub get
